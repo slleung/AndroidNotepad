2617384420514fa11215ad18688f353d27b42653
slleung <siulungleung@gmail.com>
2019-02-08 15:05:06 +0000

    RemoveButtonMessage

--- app/src/main/java/com/tcd/androidnotepad/MainActivity.kt
+++ app/src/main/java/com/tcd/androidnotepad/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.tcd.androidnotepad
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,9 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        fab.setOnClickListener {
         }
     }
 
